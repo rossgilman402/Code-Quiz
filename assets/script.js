92c219457f25a91a12f6b26f9aee08b56a933b75
ross402 <65184393+ross402@users.noreply.github.com>
2023-08-29 09:23:59 -0400

    Added comments to outline code and added eventlistener for start button and clearning main page with DOM

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,34 @@
+// DEPENDENCIES
+var mainEl = document.querySelector("#main-container");
+var startButtonEl = document.querySelector("#start-button");
 
+// VARIABLES
 
+// FUNCTIONS
+function clearMainArea() {
+  var lastChild = mainEl.lastElementChild;
+
+  // If the last child is not null remove and assign to next child
+  // keep looping until there are no more last child
+  while (lastChild) {
+    mainEl.remove(lastChild);
+    lastChild = e.lastElementChild;
+  }
+}
+
+startButtonEl.addEventListener("click", function () {
+  // Clear the current page
+  clearMainArea();
+});
+// INITAILZATIONS
+
+// Steps
+// Event listener on starting button
+// Timer starts
+// Clear the current page
+// Go through list of questions and answers
+// Maybe... next button will change question not automatically
+// Check if the question is right or wrong and take off time
+// iterate and go to the next question after answer is given
+// Once timer ends give the current score
+// Allow name to be added and save unless reset button is hit
